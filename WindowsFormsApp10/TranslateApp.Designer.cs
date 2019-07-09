fa414090cffcf796c6973b467fa2ff8b9b870176
Euronymous306 <52693808+Euronymous306@users.noreply.github.com>
2019-07-09 20:10:20 +0400

    1.0

---
+++ WindowsFormsApp10/TranslateApp.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp10
+{
+    partial class Translate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Translate));
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.ButtonSend = new System.Windows.Forms.Button();
+            this.ComboBoxFrom = new System.Windows.Forms.ComboBox();
+            this.ComboBoxTo = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TextBoxResult = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.selectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deselectAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearAllToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InputTextBox.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.InputTextBox.Location = new System.Drawing.Point(12, 57);
+            this.InputTextBox.MaxLength = 100000;
+            this.InputTextBox.Multiline = true;
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InputTextBox.Size = new System.Drawing.Size(240, 150);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputTextBox_KeyPress);
+            // 
+            // ButtonSend
+            // 
+            this.ButtonSend.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ButtonSend.Location = new System.Drawing.Point(10, 214);
+            this.ButtonSend.Name = "ButtonSend";
+            this.ButtonSend.Size = new System.Drawing.Size(523, 31);
+            this.ButtonSend.TabIndex = 2;
+            this.ButtonSend.Text = "Translate";
+            this.ButtonSend.UseVisualStyleBackColor = false;
+            this.ButtonSend.Click += new System.EventHandler(this.ButtonSend_Click);
+            // 
+            // ComboBoxFrom
+            // 
+            this.ComboBoxFrom.DisplayMember = "English";
+            this.ComboBoxFrom.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxFrom.FormattingEnabled = true;
+            this.ComboBoxFrom.Items.AddRange(new object[] {
+            "English",
+            "Russian",
+            "Georgian"});
+            this.ComboBoxFrom.Location = new System.Drawing.Point(12, 27);
+            this.ComboBoxFrom.Name = "ComboBoxFrom";
+            this.ComboBoxFrom.Size = new System.Drawing.Size(240, 24);
+            this.ComboBoxFrom.TabIndex = 3;
+            // 
+            // ComboBoxTo
+            // 
+            this.ComboBoxTo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ComboBoxTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComboBoxTo.FormattingEnabled = true;
+            this.ComboBoxTo.Items.AddRange(new object[] {
+            "English",
+            "Russian",
+            "Georgian"});
+            this.ComboBoxTo.Location = new System.Drawing.Point(291, 27);
+            this.ComboBoxTo.Name = "ComboBoxTo";
+            this.ComboBoxTo.Size = new System.Drawing.Size(240, 24);
+            this.ComboBoxTo.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(255, 22);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(35, 33);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TextBoxResult
+            // 
+            this.TextBoxResult.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextBoxResult.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.TextBoxResult.Location = new System.Drawing.Point(291, 57);
+            this.TextBoxResult.MaxLength = 100000;
+            this.TextBoxResult.Multiline = true;
+            this.TextBoxResult.Name = "TextBoxResult";
+            this.TextBoxResult.ReadOnly = true;
+            this.TextBoxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TextBoxResult.Size = new System.Drawing.Size(240, 150);
+            this.TextBoxResult.TabIndex = 7;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem,
+            this.undoToolStripMenuItem,
+            this.clearAllToolStripMenuItem,
+            this.selectAllToolStripMenuItem,
+            this.deselectAllToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(546, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.helpToolStripMenuItem.Text = "About...";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.undoToolStripMenuItem.Text = "Undo";
+            this.undoToolStripMenuItem.Click += new System.EventHandler(this.undoToolStripMenuItem_Click);
+            // 
+            // selectAllToolStripMenuItem
+            // 
+            this.selectAllToolStripMenuItem.Name = "selectAllToolStripMenuItem";
+            this.selectAllToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.selectAllToolStripMenuItem.Text = "Select All";
+            this.selectAllToolStripMenuItem.Click += new System.EventHandler(this.selectAllToolStripMenuItem_Click);
+            // 
+            // deselectAllToolStripMenuItem
+            // 
+            this.deselectAllToolStripMenuItem.Name = "deselectAllToolStripMenuItem";
+            this.deselectAllToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.deselectAllToolStripMenuItem.Text = "Deselect All";
+            this.deselectAllToolStripMenuItem.Click += new System.EventHandler(this.deselectAllToolStripMenuItem_Click);
+            // 
+            // clearAllToolStripMenuItem
+            // 
+            this.clearAllToolStripMenuItem.Name = "clearAllToolStripMenuItem";
+            this.clearAllToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.clearAllToolStripMenuItem.Text = "Clear";
+            this.clearAllToolStripMenuItem.Click += new System.EventHandler(this.clearAllToolStripMenuItem_Click);
+            // 
+            // Translate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(546, 257);
+            this.Controls.Add(this.TextBoxResult);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ComboBoxTo);
+            this.Controls.Add(this.ComboBoxFrom);
+            this.Controls.Add(this.ButtonSend);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Translate";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Translate";
+            this.Load += new System.EventHandler(this.Translate_Load);
+            this.Click += new System.EventHandler(this.Translate_Click);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.Button ButtonSend;
+        private System.Windows.Forms.ComboBox ComboBoxFrom;
+        private System.Windows.Forms.ComboBox ComboBoxTo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox TextBoxResult;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem selectAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deselectAllToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearAllToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp10/TranslateApp.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp10
+{
+    public partial class Translate : Form
+    {
+        public Translate()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSend_Click(object sender, EventArgs e)
+        {
+            if (ComboBoxFrom.SelectedIndex == 2 && InputTextBox.Text == "გამარჯობა")
+            {
+                TextBoxResult.Text = "Hello";
+            }
+            else
+            TextBoxResult.Text = InputTextBox.Text;
+        }
+
+        private void Translate_Load(object sender, EventArgs e)
+        {
+            ComboBoxFrom.SelectedIndex = 0;
+            ComboBoxTo.SelectedIndex = 0;
+        }
+
+        private void InputTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Return)
+            {
+                ButtonSend.PerformClick();
+            }
+        }
+
+        private void Translate_Click(object sender, EventArgs e)
+        {
+            InputTextBox.DeselectAll();
+        }
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Undo();
+        }
+
+        private void clearAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Clear();
+            TextBoxResult.Clear();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InputTextBox.SelectAll();
+        }
+
+        private void deselectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            InputTextBox.DeselectAll();
+        }
+    }
+}
