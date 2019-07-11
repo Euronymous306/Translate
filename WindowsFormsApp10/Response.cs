189f668f744d4840b8a2f529d5d86c1acb646c09
visual studio <vs@itstep.ge>
2019-07-11 13:39:22 +0400

    TranslationService and HttpClient Added

---
+++ WindowsFormsApp10/Response.cs
@@ -0,0 +1,56 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translate
+{
+    public class ResponseData
+    {
+        public string translatedText { get; set; }
+        public double match { get; set; }
+    }
+
+    public class Match
+    {
+        public string id { get; set; }
+        public string segment { get; set; }
+        public string translation { get; set; }
+        public string quality { get; set; }
+        public object reference { get; set; }
+
+        [JsonProperty("__invalid_name__usage-count")]
+        public double __invalid_name__usage_count { get; set; }
+
+        public string subject { get; set; }
+
+        [JsonProperty("__invalid_name__created-by")]
+        public string __invalid_name__created_by { get; set; }
+
+        [JsonProperty("__invalid_name__last-updated-by")]
+        public string __invalid_name__last_updated_by { get; set; }
+
+        [JsonProperty("__invalid_name__create-date")]
+        public string __invalid_name__create_date { get; set; }
+
+        [JsonProperty("__invalid_name__last-update-date")]
+        public string __invalid_name__last_update_date { get; set; }
+
+        public double match { get; set; }
+    }
+
+    public class RootObject
+    {
+        public ResponseData responseData { get; set; }
+        public bool quotaFinished { get; set; }
+        public object mtLangSupported { get; set; }
+        public string responseDetails { get; set; }
+        public double responseStatus { get; set; }
+        public string responderId { get; set; }
+        public object exception_code { get; set; }
+        public List<Match> matches { get; set; }
+    }
+
+}
---
+++ WindowsFormsApp10/TranslationService.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Translate
+{
+    public class TranslationService
+    {
+        string baseUlr = "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get";
+
+        public async Task<RootObject> Trasnlate(string from, string to, string text)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                    client.DefaultRequestHeaders.AcceptCharset.Add(new StringWithQualityHeaderValue("utf-8"));
+
+                    client.DefaultRequestHeaders.Add("X-RapidAPI-Host", "translated-mymemory---translation-memory.p.rapidapi.com");
+                    client.DefaultRequestHeaders.Add("X-RapidAPI-Key", "0c3f544952msh936b51bf1c8988ap193ab9jsn39ca01dd0bde");
+
+                    var result = await client.GetAsync($"{baseUlr}?langpair={from}|{to}&q={text}");
+                    var content = await result.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<RootObject>(content);
+                    return data;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+        }
+
+
+    }
+}
